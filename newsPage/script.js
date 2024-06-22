272eed9cbf5dc1ae469fc4407e50ef2120dbfe84
yuchiro <czarniknatalia16@gmail.com>
2024-06-22 21:15:48 +0200

    post class render method ;3

--- newsPage/script.js
+++ newsPage/script.js
@@ -6,14 +6,15 @@ const post = {
       text: ["listItem1", "listItem2"],
       fontSize: 24,
       fontWeight: 500,
-      color: black,
+      color: "black",
     },
 
     p: {
       text: "Lorem ipsum dolor",
       fontSize: 24,
       fontWeight: 500,
-      color: black,
+      textAlign: "center",
+      color: "black",
     },
 
     img: {
@@ -24,14 +25,44 @@ const post = {
 };
 
 class Post {
-  constructor(title, img, content) {
+  constructor(title, imgSrc, content) {
     this.title = title;
-    this.img = img;
+    this.imgSrc = imgSrc;
     this.content = content;
   }
 
   renderPost() {
     //metoda generująca "miniaturke" do posta na stronie z aktualnosciami, daje event listener do miniaturki ktory po kliknieciu bedzie wykonywal metode renderPostDetails
+    const template = document.querySelector("template");
+    const templateCopy = template.content.cloneNode(true);
+    const newsList = document.querySelector(".news");
+
+    const newsCardContainer = document.createElement("div");
+    newsCardContainer.classList.add("news-card");
+    newsCardContainer.addEventListener("click", this.renderPostDetails);
+
+    const mainImg = templateCopy.querySelector(".img-container img");
+    mainImg.src = this.imgSrc;
+    mainImg.onload = () => {
+      if (mainImg.naturalWidth > mainImg.naturalHeight) {
+        mainImg.style.setProperty("height", "100%");
+      } else {
+        mainImg.style.setProperty("width", "100%");
+      }
+    };
+
+    const title = templateCopy.querySelector(".news-title");
+    title.textContent = this.title;
+
+    const shortText = templateCopy.querySelector(".news-short-text");
+    if (this.content.p.text.split("").length < 50) {
+      shortText.textContent = this.content.p.text;
+    } else {
+      shortText.textContent = this.content.p.text.substring(0, 50) + "...";
+    }
+
+    newsCardContainer.appendChild(templateCopy);
+    newsList.insertAdjacentElement("afterbegin", newsCardContainer);
   }
 
   renderPostDetails() {
@@ -50,3 +81,143 @@ const createList = (listProperties) => {
 const createImg = (imgProperties) => {
   //funkcja generująca obrazek
 };
+
+const posts = [
+  new Post("cool post title ;3:3;3:3", "images/post-images/pxfuel (7).jpg", {
+    p: {
+      text: "Lorem ipsum dolor sit amet consectetu",
+      fontSize: 24,
+      fontWeight: 500,
+      textAlign: "center",
+      color: "black",
+    },
+
+    list: {
+      text: ["listItem1", "listItem2"],
+      fontSize: 24,
+      fontWeight: 500,
+      color: "black",
+    },
+
+    img: {
+      src: "postImages/image.png",
+      width: 70,
+    },
+  }),
+
+  new Post("second cool post", "images/post-images/pxfuel (8).jpg", {
+    list: {
+      text: ["listItem1", "listItem2"],
+      fontSize: 24,
+      fontWeight: 500,
+      color: "black",
+    },
+
+    img: {
+      src: "postImages/image.png",
+      width: 70,
+    },
+
+    p: {
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      fontSize: 24,
+      fontWeight: 500,
+      textAlign: "center",
+      color: "black",
+    },
+  }),
+  new Post("cool post title", "images/post-images/pxfuel (10).jpg", {
+    p: {
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      fontSize: 24,
+      fontWeight: 500,
+      textAlign: "center",
+      color: "black",
+    },
+
+    list: {
+      text: ["listItem1", "listItem2"],
+      fontSize: 24,
+      fontWeight: 500,
+      color: "black",
+    },
+
+    img: {
+      src: "postImages/image.png",
+      width: 70,
+    },
+  }),
+
+  new Post("second cool post", "images/post-images/pxfuel (9).jpg", {
+    list: {
+      text: ["listItem1", "listItem2"],
+      fontSize: 24,
+      fontWeight: 500,
+      color: "black",
+    },
+
+    img: {
+      src: "postImages/image.png",
+      width: 70,
+    },
+
+    p: {
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      fontSize: 24,
+      fontWeight: 500,
+      textAlign: "center",
+      color: "black",
+    },
+  }),
+  new Post(
+    "cool post title",
+    "images/post-images/MPKT2024-Insta-stories-edited_1.png",
+    {
+      p: {
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+        fontSize: 24,
+        fontWeight: 500,
+        textAlign: "center",
+        color: "black",
+      },
+
+      list: {
+        text: ["listItem1", "listItem2"],
+        fontSize: 24,
+        fontWeight: 500,
+        color: "black",
+      },
+
+      img: {
+        src: "postImages/image.png",
+        width: 70,
+      },
+    }
+  ),
+
+  new Post("second cool post", "images/post-images/pxfuel (8).jpg", {
+    list: {
+      text: ["listItem1", "listItem2"],
+      fontSize: 24,
+      fontWeight: 500,
+      color: "black",
+    },
+
+    img: {
+      src: "postImages/image.png",
+      width: 70,
+    },
+
+    p: {
+      text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      fontSize: 24,
+      fontWeight: 500,
+      textAlign: "center",
+      color: "black",
+    },
+  }),
+];
+
+posts.forEach((post) => {
+  post.renderPost();
+});
