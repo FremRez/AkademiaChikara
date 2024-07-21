4c3634a6e9ab53d4913fba196b0fb6b4b35eb0be
yuchiro <czarniknatalia16@gmail.com>
2024-07-21 12:28:58 +0200

    camp details

--- campPage/script.js
+++ campPage/script.js
@@ -1,43 +1,73 @@
 import renderRoot from "../root.js";
 
-const images = document.querySelectorAll(".scrolling-images img")
+const images = document.querySelectorAll(".scrolling-images img");
 
 function hideImage() {
-    images.forEach(image =>{
-        if (image.className == "image3") {
-            setTimeout(function() {
-                image.style.setProperty("display", "none")
-            }, 500); 
-        }
-        if (image.className == "image4") {
-            setTimeout(function() {
-                image.style.setProperty("display", "inline-block")
-            }, 500); 
-        }
-    });
+  images.forEach((image) => {
+    if (image.className == "image3") {
+      setTimeout(function () {
+        image.style.setProperty("display", "none");
+      }, 500);
+    }
+    if (image.className == "image4") {
+      setTimeout(function () {
+        image.style.setProperty("display", "inline-block");
+      }, 500);
+    }
+  });
 }
 
 function imageSlide() {
-    images.forEach(image => {
-        let imageClass = image.className;
-        let number = [...imageClass];
-        let lastNumber = parseInt(number.pop());
-    
-        if (lastNumber == 5) {
-            lastNumber = 0
-        }
+  images.forEach((image) => {
+    let imageClass = image.className;
+    let number = [...imageClass];
+    let lastNumber = parseInt(number.pop());
 
-        lastNumber++
-    
-        image.className = ''
-        image.classList.add("image" + lastNumber)
-    });
+    if (lastNumber == 5) {
+      lastNumber = 0;
+    }
 
-    hideImage()
+    lastNumber++;
+
+    image.className = "";
+    image.classList.add("image" + lastNumber);
+  });
+
+  hideImage();
 }
 
-hideImage()
+hideImage();
 
 setInterval(imageSlide, 5000);
+
+const closeDetailsButton = document.querySelectorAll(".close-details");
+const blackBackgroung = document.querySelector(".black-bcg");
+const campDetails = document.querySelectorAll(".event-details");
+const currentEvents = document.querySelectorAll(".event-item");
+
+const showEvents = (index) => {
+  index++;
+  campDetails.forEach((camp) => {
+    if (!camp.classList.contains("camp" + index)) return;
+    camp.classList.remove("hidden");
+    blackBackgroung.classList.remove("hidden");
+  });
+};
+
+closeDetailsButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    campDetails.forEach((eventWindow) => {
+      eventWindow.classList.add("hidden");
+    });
+    blackBackgroung.classList.add("hidden");
+  });
+});
+
+currentEvents.forEach((eventDetails, i) => {
+  console.log(i);
+  eventDetails.addEventListener("click", () => {
+    showEvents(i);
+  });
+});
 
 renderRoot("campPageLink");
